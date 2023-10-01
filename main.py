bdc5554338c556fc8664aa9d881f384ea943d753
maks <verdena2020@gmail.com>
2023-10-01 13:36:45 +0300

    class

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hell Me")
+class Student():
+    def __ini__(self,name,surname,age,logiks,group,login,password,telegram):
+        self.name =name
+        self.surname = surname
+        self.age = age
+        self.logiks = logiks
+        self.group = group
+        self.login = login
+        self.password = password
+        self.telegram = telegram
+
+    def all_info(self):
+        print("Name"self.name )
+        print("Surname"self.surname )
+        print("Age"self.age)
+        print("Logiks"self.logiks )
+        print("Group"self.group )
+        print("Login"self.login )
+        print("Password"self.password )
+        print("Telegram"self.telegram )
+        
+
+
+
+   
